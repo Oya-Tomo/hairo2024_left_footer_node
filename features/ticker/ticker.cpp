18fbfacb0f483e8ed719eb64625cd7168f48a7a0
oyatomo <82521012+Oya-Tomo@users.noreply.github.com>
2024-12-05 18:34:23 +0900

    update: add frame rate & dt getter

--- features/ticker/ticker.cpp
+++ features/ticker/ticker.cpp
@@ -1,6 +1,8 @@
 #include "ticker.h"
 
-static uint32_t ms_per_frame = 1000 / 20;
+const int FRAME_RATE = 20;
+
+static uint32_t ms_per_frame = 1000 / FRAME_RATE;
 static bool in_frame = false;
 
 int64_t break_frame_wait_callback(alarm_id_t id, void *user_data)
@@ -17,4 +19,14 @@ void frame_wait()
     }
     in_frame = true;
     add_alarm_in_ms(ms_per_frame, break_frame_wait_callback, NULL, false);
+}
+
+double get_frame_rate()
+{
+    return FRAME_RATE;
+}
+
+double get_dt()
+{
+    return 1.0 / FRAME_RATE;
 }
--- features/ticker/ticker.h
+++ features/ticker/ticker.h
@@ -5,3 +5,7 @@
 int64_t break_frame_wait_callback(alarm_id_t id, void *user_data);
 
 void frame_wait();
+
+double get_frame_rate();
+
+double get_dt();
