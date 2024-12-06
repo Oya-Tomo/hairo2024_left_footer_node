9d69eaa99549d38edef5059c7caea92c9dca7392
oyatomo <82521012+Oya-Tomo@users.noreply.github.com>
2024-12-06 09:32:14 +0900

    update: fixed pin var name

--- features/connection/connection.cpp
+++ features/connection/connection.cpp
@@ -38,7 +38,7 @@ const uint8_t I2C_SLAVE_ADDRESS = 0x10;
 const uint8_t I2C_BAUDRATE = 100 * 1000;
 
 const uint I2C_SDA_PIN = 16;
-const uint I2C_SCL = 17;
+const uint I2C_SCL_PIN = 17;
 
 void connection_setup()
 {
@@ -46,9 +46,9 @@ void connection_setup()
     gpio_set_function(I2C_SDA_PIN, GPIO_FUNC_I2C);
     gpio_pull_up(I2C_SDA_PIN);
 
-    gpio_init(I2C_SCL);
-    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C_SCL);
+    gpio_init(I2C_SCL_PIN);
+    gpio_set_function(I2C_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SCL_PIN);
 
     i2c_init(i2c0, I2C_BAUDRATE);
     i2c_slave_init(i2c0, I2C_SLAVE_ADDRESS, &i2c_slave_handler);
